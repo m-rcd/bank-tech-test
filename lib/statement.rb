8a89a0b7125b11d74494a5079233fab82a1be01e
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-05 15:39:31 +0000

    can print account statement after withdrawing money

--- lib/statement.rb
+++ lib/statement.rb
@@ -7,8 +7,12 @@ class Statement
   def print_statement(transactions)
     statement = header
     transactions.each do |transaction|
-      statement += "\n #{transaction[:date]} || #{transaction[:credit]} || || #{transaction[:balance] }"
+      if transaction[:type] == 'credit'
+        statement += "\n #{transaction[:date]} || #{transaction[:credit]} || || #{transaction[:balance] }"
+      elsif transaction[:type] == 'debit'
+        statement += " \n #{transaction[:date]} || || #{transaction[:debit]} || #{transaction[:balance] }"
+      end
     end
-    return statement
-  end
+   return statement
+ end
 end
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -11,14 +11,14 @@ describe 'User can make transactions' do
     end
   end
 
-    context 'Withdraw' do
-      it 'should be able to Withdraw money' do
-        account = Account.new
-        account.deposit(1000)
-        account.withdraw(500)
-        expect(account.balance).to eq(500)
-      end
+  context 'Withdraw' do
+    it 'should be able to Withdraw money' do
+      account = Account.new
+      account.deposit(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq(500)
     end
+  end
 end
 
 describe 'Account statement' do
@@ -29,14 +29,25 @@ describe 'Account statement' do
       expect(statement.print_statement(account.transactions)).to eq('Date || Credit || Debit || Balance ')
     end
   end
-  
+
   context 'After a deposit' do
-    it 'should be able to print statement' do
+    it 'should be able to print statement with deposit transaction' do
       account = Account.new
       allow(Date).to receive(:today).and_return(Date.parse('19/09/2018'))
       account.deposit(1000)
       statement = Statement.new
       expect(statement.print_statement(account.transactions)).to eq("Date || Credit || Debit || Balance \n 19/09/2018 || 1000.00 || || 1000.00")
+    end
+  end
+
+  context 'After a withdrawal' do
+    it 'should be able to print statement with deposit and withdrawal transactions' do
+      account = Account.new
+      allow(Date).to receive(:today).and_return(Date.parse('19/09/2018'))
+      account.deposit(1000)
+      account.withdraw(500)
+      statement = Statement.new
+      expect(statement.print_statement(account.transactions)).to eq("Date || Credit || Debit || Balance \n 19/09/2018 || 1000.00 || || 1000.00 \n 19/09/2018 || || 500.00 || 500.00")
     end
   end
 end
