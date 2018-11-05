2afa5b60214307c96e22d2e4097da2bff7b6f737
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-05 12:47:40 +0000

    refactor feature test

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -2,19 +2,21 @@
 
 require 'account'
 
-describe 'Deposit' do
-  it 'should be able to deposit money in account' do
-    account = Account.new
-    account.deposit(1000)
-    expect(account.balance).to eq(1000)
-  end
-
-  describe 'Withdraw' do
-    it 'should be able to Withdraw money' do
+describe 'User can make transactions' do
+  context 'Deposit' do
+    it 'should be able to deposit money in account' do
       account = Account.new
       account.deposit(1000)
-      account.withdraw(500)
-      expect(account.balance).to eq(500)
+      expect(account.balance).to eq(1000)
     end
   end
+
+    context 'Withdraw' do
+      it 'should be able to Withdraw money' do
+        account = Account.new
+        account.deposit(1000)
+        account.withdraw(500)
+        expect(account.balance).to eq(500)
+      end
+    end
 end
