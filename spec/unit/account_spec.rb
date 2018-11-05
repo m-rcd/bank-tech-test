f48d4a1a4ad42f0e0126c9d4a32d2eed49da2101
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-05 12:01:41 +0000

    refactor test

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -1,27 +1,25 @@
 require 'account'
 
-describe 'Account' do
+describe Account do
+  subject(:account) { described_class.new }
+  
   context '#initialize' do
     it 'starts with an empty balance' do
-      account = Account.new
       expect(account.balance).to eq(0)
     end
 
     it 'starts with an empty actions array' do
-      account = Account.new
       expect(account.transactions).to be_empty
     end
   end
 
   context '#deposit' do
     it 'should increase balance' do
-      account = Account.new
       account.deposit(1000)
       expect(account.balance).to eq(1000)
     end
 
     it 'should add the transactions to transactions array' do
-      account = Account.new
       allow(Date).to receive(:today).and_return(Date.parse("19/09/2018"))
       account.deposit(1000)
       expect(account.transactions).to include({ date: '19/09/2018', credit: 1000, balance: 1000 })
